c1588ac640eb70300ddebcae77ee20beca57b7e8
aman9508 <60673091+aman9508@users.noreply.github.com>
2020-04-01 17:58:06 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Amanpreet kaur</h1>
+    <h2>My Hosting</h2>
+    <h3>Welcome to my first page</h3>
+</body>
+</html>
